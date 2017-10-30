1ee52e1323a2fe31f5ff31f3bc8788a8fd115ed7
JustinZZW <zhiwei92@163.com>
2017-10-30 16:14:56 +0800

    insert the cat information, and add output csv function in Info_Cal

--- R/Info_Cal.R
+++ R/Info_Cal.R
@@ -3,12 +3,14 @@
 #' @author Zhiwei Zhou
 #' \email{zhouzw@@sioc.ac.cn}
 #' @param raw.data The smiles structure as vector format.
+#' @param is.output Whether output the csv file? The default is TRUE.
 #' @return a matrix (n*3) including formula, exact mass, and charge
 #' @example Info_Cal(raw.data=raw.data)
 
-Info_Cal <- function(raw.data){
+Info_Cal <- function(raw.data, is.output=TRUE){
   temp <- rcdk::parse.smiles(raw.data)
 
+  cat("Start calculate the Formula and Exact mass\n")
   formula.info <- lapply(seq(length(temp)), function(i){
     if (i %in% seq(length(temp), length.out = 11)) {
       cat(round((i/length(temp))*100, digits = 2)); cat("%"); cat(" ")
@@ -29,5 +31,13 @@ Info_Cal <- function(raw.data){
 
   result <- do.call(rbind, formula.info)
   result <- data.frame(formula=result[,1], exact.mass=result[,2], charge=result[,3])
+
+  if (is.output==TRUE) {
+    cat("Start output result\n")
+    temp <- paste("info", "csv", sep = ".")
+    write.csv(result, temp, row.names = F)
+  }
+
   return(result)
+
 }
