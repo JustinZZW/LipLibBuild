ac64c2f26a9afa993242e7aebba1a0abafaf6b9d
JustinZZW <zhiwei92@163.com>
2017-12-07 17:12:38 +0800

    add a funtion of average_int

---
+++ R/average_int.R
@@ -0,0 +1,43 @@
+#' @title average_int
+#' @author Zhiwei Zhou
+#' @description Averge intensity of same fragment in multiple file.
+#' @param dir.path the files direction path
+
+average_int <- function(dir.path="."){
+  raw.data <- lapply(seq(length(dir(dir.path))), function(i){
+    temp <- readr::read_csv(dir()[i])
+  })
+
+  all.annotation <- sapply(raw.data, function(x){
+    x$annotation
+  })
+
+  all.annotation <- do.call(c, all.annotation)
+  all.annotation <- unique(all.annotation)
+
+  int.result <- lapply(seq(length(all.annotation)), function(i){
+    temp <- all.annotation[i]
+    int <- sapply(seq(length(raw.data)), function(i){
+      search.data <- raw.data[[i]]$annotation
+      idx <- which(search.data==temp)
+      if (length(idx) > 0){
+        result <- raw.data[[i]]$int[idx]
+      } else {
+        result <- NA
+      }
+      return(result)
+    })
+    return(int)
+  })
+
+  names(int.result) <- all.annotation
+  avg.result <- lapply(int.result, function(x){
+    round(mean(x, na.rm = T), digits = 0)
+  })
+  avg.result <- do.call(c, avg.result)
+  avg.result <- data.frame(annotation=all.annotation,
+                           int=avg.result,
+                           stringsAsFactors = F)
+
+  write.csv(avg.result, "intensity template.csv", row.names = F)
+}
